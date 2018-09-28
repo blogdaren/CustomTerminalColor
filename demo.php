dd3fafc3c04b03a45945fbfa038eb3966d9d2d25
blogdaren <blogdaren@163.com>
2018-09-28 16:12:30 +0800

    update README.md

--- demo.php
+++ demo.php
@@ -7,16 +7,20 @@ use CustomTerminalColor\Color;
 echo PHP_EOL;
 
 //level: info
-Color::showInfo('1. http://www.blogdaren.com [Info]' . PHP_EOL . PHP_EOL);
+Color::showInfo('1. http://www.blogdaren.com [Info]');
+echo PHP_EOL . PHP_EOL;
 
 //level: warning
-Color::showWarning('2. http://www.blogdaren.com [Warning]' . PHP_EOL.PHP_EOL);
+Color::showWarning('2. http://www.blogdaren.com [Warning]');
+echo PHP_EOL . PHP_EOL;
 
 //level: error
-Color::showError('3. http://www.blogdaren.com [Error]' . PHP_EOL . PHP_EOL);
+Color::showError('3. http://www.blogdaren.com [Error]');
+echo PHP_EOL . PHP_EOL;
 
 //level: custom
-Color::show('4. http://www.blogdaren.com [Custom]' . PHP_EOL . PHP_EOL, 'yellow', 'blue');
+Color::show('4. http://www.blogdaren.com [Custom]');
+echo PHP_EOL . PHP_EOL;
 
 //get colored string and show it
 echo Color::getColorfulText('5. http://www.blogdaren.com [Fetch]', 'white', 'cyan');
@@ -29,3 +33,4 @@ echo PHP_EOL . PHP_EOL;
 //get colored & underlined string and show it
 echo Color::getColorfulText('7. http://www.blogdaren.com [Underline]', 'white', 'magenta', 'underline');
 echo PHP_EOL . PHP_EOL;
+
