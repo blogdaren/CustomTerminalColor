3135d5cf54277510f466152f0fb4d6ceead5cd45
blogdaren <blogdaren@163.com>
2025-04-09 13:17:51 +0800

    domain changes

--- demo.php
+++ demo.php
@@ -7,30 +7,30 @@ use CustomTerminalColor\Color;
 echo PHP_EOL;
 
 //level: info
-Color::showInfo('1. http://www.blogdaren.com [Info]');
+Color::showInfo('1. http://www.phpcreeper.com [Info]');
 echo PHP_EOL . PHP_EOL;
 
 //level: warning
-Color::showWarning('2. http://www.blogdaren.com [Warning]');
+Color::showWarning('2. http://www.phpcreeper.com [Warning]');
 echo PHP_EOL . PHP_EOL;
 
 //level: error
-Color::showError('3. http://www.blogdaren.com [Error]');
+Color::showError('3. http://www.phpcreeper.com [Error]');
 echo PHP_EOL . PHP_EOL;
 
 //level: custom
-Color::show('4. http://www.blogdaren.com [Custom]');
+Color::show('4. http://www.phpcreeper.com [Custom]');
 echo PHP_EOL . PHP_EOL;
 
 //get colorful text and show it
-$text = Color::getColorfulText('5. http://www.blogdaren.com [Fetch]', 'white', 'cyan');
+$text = Color::getColorfulText('5. http://www.phpcreeper.com [Fetch]', 'white', 'cyan');
 echo $text . PHP_EOL . PHP_EOL;
 
 //get colorful & blinked text and show it
-$text = Color::getColorfulText('6. http://www.blogdaren.com [Blink]', 'light_blue', 'magenta', array('blink'));
+$text = Color::getColorfulText('6. http://www.phpcreeper.com [Blink]', 'light_blue', 'magenta', array('blink'));
 echo $text . PHP_EOL . PHP_EOL;
 
 //get colorful & underlined text and show it
-$text = Color::getColorfulText('7. http://www.blogdaren.com [Underline]', 'white', 'magenta', 'underline');
+$text = Color::getColorfulText('7. http://www.phpcreeper.com [Underline]', 'white', 'magenta', 'underline');
 echo $text . PHP_EOL . PHP_EOL;
 
